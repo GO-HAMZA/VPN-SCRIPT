6c0f654f555d3fe3f998fe27608bd694e51297f9
hamzascript <165971938+hamzascript@users.noreply.github.com>
2025-08-02 06:54:42 +0100

    Update HAMZA-VPN.sh

--- HAMZA-VPN.sh
+++ HAMZA-VPN.sh
@@ -220,7 +220,7 @@ edit_banner() {
 
 install_dns() {
   echo "Installing HAMZA DNS script..."
-  wget -O /tmp/dns.sh https://raw.githubusercontent.com/hamzascript/X-SCRIPT/main/hamza-dns.sh
+  curl -O /tmp/dns.sh https://raw.githubusercontent.com/hamzascript/X-SCRIPT/main/hamza-dns.sh
   chmod +x /tmp/dns.sh && bash /tmp/dns.sh
   echo "DNS Script installed."
 }
